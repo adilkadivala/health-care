5d739dd92e1e10e2a096395454d57fe2f9b06325
adilkadivala <adilkadivala560@gmail.com>
2026-04-20 12:53:49 +0530

    resolving deployment error

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,18 @@
-import env from "env";
-env.configure();
-
-
 import express from "express";
 import cors from "cors";
-import path from "node:path";
 import routes from "./routes";
+
+// Load .env ONLY in development
+if (process.env.NODE_ENV !== "production") {
+  await import("dotenv/config");
+}
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.use("/api", routes);
 app.use(routes);
@@ -21,11 +21,9 @@ app.get("/", (req, res) => {
   res.send("Hello from Health Care server!");
 });
 
-if(process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
+// Always listen (Render needs this)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default app;
-export const handler = app;
